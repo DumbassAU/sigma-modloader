71e73203bb246e6e9c8190bda171e6450a849fb7
DumbassAU <yulyev.sultan@yandex.ru>
2025-07-25 20:22:33 +0500

    closed source

--- Kliko's modloader/modules/filesystem/exceptions.py
+++
@@ -1,4 +0,0 @@
-class TrailingDotError(OSError): pass
-class EmptyFileNameError(OSError): pass
-class InvalidFileNameError(OSError): pass
-class ReservedFileNameError(OSError): pass
--- Kliko's modloader/modules/frontend/launcher/dataclasses/__init__.py
+++
@@ -1,2 +0,0 @@
-from .window_config import WindowConfig
-from .widget_config import WidgetConfig
